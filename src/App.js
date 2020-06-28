2e3ae517eefee5feb6cf6482591ea7768a01bb00
JuanPappalardo <juan.gp7@hotmail.com>
2020-06-27 21:30:17 -0300

    create react app + add mapbox

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import ReactMapGL, { Marker } from "react-map-gl";
+import { FARMACIAS } from "./data/listado";
 
 function App() {
+  const [viewport, setViewport] = useState({
+    latitude: -34.816667,
+    longitude: -58.283333,
+    width: "100vw",
+    height: "100vh",
+    zoom: 10,
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        onViewportChange={(viewport) => {
+          setViewport(viewport);
+        }}
+        mapStyle="mapbox://styles/juanpappalardo/ckby1iqsp0sgu1ilcgbk0hp1v"
+      >
+        {FARMACIAS.map((farmacia) => (
+          <Marker
+            key={farmacia.establecimiento_id}
+            latitude={Number(farmacia.lat)}
+            longitude={Number(farmacia.long)}
+          >
+            <img className="farma-btn" src="images/medicine.svg" />
+          </Marker>
+        ))}
+      </ReactMapGL>
     </div>
   );
 }
